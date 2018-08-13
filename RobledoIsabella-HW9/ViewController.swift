4b78c1e743facea119a3f97181b46d08118ac616
Robledo <ibr79@lib-pclem055.austin.utexas.edu>
2018-08-13 17:48:36 -0500

    some stuff

--- RobledoIsabella-HW9/ViewController.swift
+++ RobledoIsabella-HW9/ViewController.swift
@@ -9,10 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 41, height: 35))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        
+        createLabel()
+        initializeGestures()
+        
+       
+    }
+    
+    func initializeGestures(){
+        
+        let swipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(recognizeSwipeGesture(recognizer:)))
+        swipeRecognizer.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRecognizer)
+        
+//        let edgePanRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(recognizeEdgePanGesture(recognizer:)))
+//        edgePanRecognizer.edges = UIRectEdge.left
+//        self.view.addGestureRecognizer(edgePanRecognizer)
+    }
+    
+    
+    func createLabel(){
+        // CGRectMake has been deprecated - and should be let, not var
+        //let label = UILabel(frame: CGRect(x: 0, y: 0, width: 41, height: 35))
+        // you will probably want to set the font (remember to use Dynamic Type!)
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        // and set the text color too - remember good contrast
+        label.textColor = .black
+        label.backgroundColor = .green
+        label.center = self.view.center
+        // this changed in Swift 3 (much better, no?)
+        label.textAlignment = .center
+        label.text = ""
+        self.view.addSubview(label)
+    }
+    
+    @IBAction func recognizeSwipeGesture(recognizer: UISwipeGestureRecognizer)
+    {
+        if recognizer.state == .ended {
+            label.text = "Swipe"
+            
+            self.label.center.x = self.view.center.x
+            
+            UIView.animate (withDuration: 1.0,
+                            animations: {
+                                self.label.center.x +=    self.view.bounds.width
+            })
+            
+            self.label.center.x = self.view.center.x - self.view.bounds.width
+            
+            UIView.animate (withDuration: 1.0,
+                            animations: {
+                                self.label.center.x += self.view.bounds.width
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
