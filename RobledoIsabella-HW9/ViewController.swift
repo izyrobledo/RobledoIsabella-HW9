6bc2eb020d4c7a87aaf032ed1d5fbe5353b1d9ee
Robledo <ibr79@lib-pclem045.austin.utexas.edu>
2018-08-14 12:38:15 -0500

    latest

--- RobledoIsabella-HW9/ViewController.swift
+++ RobledoIsabella-HW9/ViewController.swift
@@ -10,64 +10,108 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 41, height: 35))
-
+    //let screenSize = UIScreen.main.bounds
+    //let screenSize = UIScreen.main.bounds
+    let boxHeight = UIScreen.main.bounds.height/19
+    let boxWidth = UIScreen.main.bounds.width/9
+    
+    var label: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.viewDidLoad()
-        
         createLabel()
         initializeGestures()
         
-       
     }
     
     func initializeGestures(){
         
-        let swipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(recognizeSwipeGesture(recognizer:)))
-        swipeRecognizer.direction = UISwipeGestureRecognizerDirection.right
-        self.view.addGestureRecognizer(swipeRecognizer)
+//        let directions: [UISwipeGestureRecognizerDirection] = [.right, .left, .up, .down]
+//        for direction in directions {
+//            let gesture = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipe:"))
+//            gesture.direction = direction
+//            self.view.addGestureRecognizer(gesture)
+//        }
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(recognizeSwipeGesture(recognizer:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
         
-//        let edgePanRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(recognizeEdgePanGesture(recognizer:)))
-//        edgePanRecognizer.edges = UIRectEdge.left
-//        self.view.addGestureRecognizer(edgePanRecognizer)
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(recognizeSwipeGesture(recognizer:)))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(recognizeSwipeGesture(recognizer:)))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(recognizeSwipeGesture(recognizer:)))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        self.view.addGestureRecognizer(swipeDown)
     }
     
     
     func createLabel(){
-        // CGRectMake has been deprecated - and should be let, not var
-        //let label = UILabel(frame: CGRect(x: 0, y: 0, width: 41, height: 35))
-        // you will probably want to set the font (remember to use Dynamic Type!)
-        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+       
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: boxWidth, height: boxHeight))
+        print ("boxWidth: \(boxWidth)")
+        print ("boxWidth: \(boxWidth)")
+        label?.font = UIFont.preferredFont(forTextStyle: .footnote)
         // and set the text color too - remember good contrast
-        label.textColor = .black
-        label.backgroundColor = .green
-        label.center = self.view.center
+        label?.textColor = .black
+        label?.backgroundColor = .green
+        label?.center = self.view.center
         // this changed in Swift 3 (much better, no?)
-        label.textAlignment = .center
-        label.text = ""
-        self.view.addSubview(label)
+        label?.textAlignment = .center
+        label?.text = "start"
+        self.view.addSubview(label!)
     }
     
     @IBAction func recognizeSwipeGesture(recognizer: UISwipeGestureRecognizer)
     {
-        if recognizer.state == .ended {
-            label.text = "Swipe"
-            
-            self.label.center.x = self.view.center.x
-            
-            UIView.animate (withDuration: 1.0,
-                            animations: {
-                                self.label.center.x +=    self.view.bounds.width
-            })
-            
-            self.label.center.x = self.view.center.x - self.view.bounds.width
-            
-            UIView.animate (withDuration: 1.0,
-                            animations: {
-                                self.label.center.x += self.view.bounds.width
-            })
+        if let swipeGesture = recognizer as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                label?.text = "right"
+                print(label?.center.x)
+                print(label?.center.y)
+                
+            case UISwipeGestureRecognizerDirection.down:
+                label?.text = "down"
+            case UISwipeGestureRecognizerDirection.left:
+                label?.text = "left"
+            case UISwipeGestureRecognizerDirection.up:
+                label?.text = "up"
+            default:
+                break
+            }
         }
+        
+//        if recognizer.state == .ended {
+//            label.text = "Swipe"
+//
+//            self.label.center.x = self.view.center.x
+//
+//            UIView.animate (withDuration: 1.0,
+//                            animations: {
+//                                self.label.center.x +=    self.view.bounds.width
+//            })
+//
+//            self.label.center.x = self.view.center.x - self.view.bounds.width
+//
+//            UIView.animate (withDuration: 1.0,
+//                            animations: {
+//                                self.label.center.x += self.view.bounds.width
+//            })
+//        }
+    }
+    
+    @IBAction func recognizeTapGesture(recognizer: UITapGestureRecognizer)
+    {
+        label?.text = "Tap"
+        let colorRed: CGFloat = CGFloat(arc4random()) / CGFloat(RAND_MAX)
+        let colorGreen: CGFloat = CGFloat(arc4random()) / CGFloat(RAND_MAX)
+        let colorBlue: CGFloat = CGFloat(arc4random()) / CGFloat(RAND_MAX)
+        label?.backgroundColor = UIColor(red: colorRed, green: colorGreen, blue: colorBlue, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
